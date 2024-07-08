ee8e3b076d3ec7b8ddfae3e38d80186c50c8d47a
Sandra Sosa <sosasandramabel@gmail.com>
2024-07-08 09:51:37 -0300

    cambio_dMerge

--- bbdd.js
+++ bbdd.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "tpartevivo",
+  host: "bghj9p2xcrrhmyb5xujw-mysql.services.clever-cloud.com",
+  user: "uajartje2oigcr4q",
+  password: "JOCwI0cR7JB0aCuWcd2G",
+  database: "bghj9p2xcrrhmyb5xujw",
 });
 
 connection.connect();
